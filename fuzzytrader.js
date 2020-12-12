45522092e47d1a6721bdc9f51687dd6ad2e1f35c
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2020-12-12 16:51:31 -0300

    Update fuzzytrader.js

--- fuzzytrader.js
+++ fuzzytrader.js
@@ -5,9 +5,12 @@ function getOrdersForAmount(amount, callback) {
 	if (amount <= 0) return []
 	
 	callback([
-		{symbol:"AAPL", quantity:(amount/135.66).toFixed(), price:"135.66"},
-		{symbol:"BNB", quantity:(amount/183.05).toFixed(5), price:"183.05"},
-		{symbol:"VALE", quantity:(amount/55.0).toFixed(), price:"55.0"}
+		{symbol:"AAPL", quantity:(amount/135.66).toFixed(), price:"135.66",
+		 	description:"Apple Inc. - Technology", type:"stock", profile:"conservative"},
+		{symbol:"BNB", quantity:(amount/183.05).toFixed(5), price:"183.05",
+		 	description:"Binance Exchange cryptocurrency", type:"crypto", profile:"conservative"},
+		{symbol:"VALE", quantity:(amount/55.0).toFixed(), price:"55.0",
+		 	description:"Vale S.A. - Iron ore, nickel", type:"stock", profile:"conservative"}
 	])
 }
 
@@ -15,8 +18,8 @@ function getPortfolio(callback) {
 	callback({
 		total_amount:"239,034.23",
 		assets: [
-			{symbol:"KODK", quantity:"9876.66", price:"135.66", amount:"567,855.0"},
-			{symbol:"BTCUSDT", quantity:"7218.05", price:"183.05", amount:"23,423.20"},
+			{symbol:"KODK", quantity:"9876", price:"135.66", amount:"567,855.0"},
+			{symbol:"BTCUSDT", quantity:"18.05", price:"183.05", amount:"23,423.20"},
 			{symbol:"AAPL", quantity:"45000", price:"183.05", amount:"500,000.05"},
 			{symbol:"XRP", quantity:"0.074657", price:"567855.0", amount:"855.0"}
 		]
