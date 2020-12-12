dd6f78cf2be925078d7d7b75fcfaea662ed4df81
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2020-12-12 17:10:46 -0300

    Update fuzzytrader.test.js

--- test/fuzzytrader.test.js
+++ test/fuzzytrader.test.js
@@ -21,9 +21,31 @@ beforeAll(done => {
 
 describe('Portfolio', () => {
 	test('empty', done => {
+		console.log(process.env.NODE_ENV)
 		fuzzy.getPortfolio((portfolio) => {
 			expect(portfolio.assets.length).toBe(4)
 			expect(portfolio.total_amount).toBe(239034.23)
+			done()
+		})
+	})
+})
+
+describe('Orders', () => {
+	test('empty', done => {
+		fuzzy.getOrdersForAmount(1000, (orders) => {
+			expect(orders.assets.length).toBe(3)
+			
+			expect(orders[0].symbol).toBe('AAPL')
+			expect(orders[0].quantity).toBe(1000 / 135.66))
+			expect(orders[0].price).toBe(135.66)
+			
+			expect(orders[1].symbol).toBe('BNB')
+			expect(orders[1].quantity).toBe(1000 / 183.05))
+			expect(orders[1].price).toBe(183.05)
+			
+			expect(orders[2].symbol).toBe('VALE')
+			expect(orders[2].quantity).toBe(1000 / 55.0))
+			expect(orders[2].price).toBe(55.0)
 			done()
 		})
 	})
