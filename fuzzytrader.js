faebacab1ff162d5ee42dedc0db120fb583f52e8
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2020-12-12 21:26:10 -0300

    Update fuzzytrader.js

--- fuzzytrader.js
+++ fuzzytrader.js
@@ -1,5 +1,5 @@
-const assets = require('./assets')
-const db = require('./db')
+const assetsModule = require('./assets')
+const dbModule = require('./db')
 
 function getOrdersForAmount(amount, callback) {
 	if (amount <= 0) return []
@@ -14,31 +14,35 @@ function getOrdersForAmount(amount, callback) {
 	])
 }
 
+// Lists available assets, sets price, sets the quantity owned by the trader (in his portfolio),
+// calculateS each amount and then return it back through the callback function passed as a parameter.
 function getPortfolio(callback) {
-	db.listAssets((assets) => {
+	dbModule.listAssets((assets) => {
+		// mounts the portfolio JSON
+		let jsonPortfolio = {total_amount: 0}
+		jsonPortfolio.assets = assets
+		
 		// sets quantity.
-		db.listPortfolio((assetsPortfolio) => {
+		dbModule.listPortfolio((assetsPortfolio) => {
 			assets.forEach((asset) => {
 				// sets the 'quantity' property of each asset available.
-				if (assetsPortfolio[asset.symbol])
-					asset.quantity = assetsPortfolio[asset.symbol].quantity
-				else asset.quantity = 0
+				let assetPortfolio = assetsPortfolio[asset.symbol]
+				if (assetPortfolio) {
+					asset.quantity = assetPortfolio.quantity
+					// sets price and amount
+					getPrice(asset, (price) => {
+						asset.price = price
+						asset.amount = asset.quantity * price
+						jsonPortfolio.total_amount += asset.amount
+					})
+					callback(json)
+				} else {
+					asset.quantity = 0
+					asset.amount = 0
+				}
 			})
 		})
-		
-		// sets price.
-		
-		// sets amount.
-		let total = 0
-		assets.forEach((asset) => {
-			let amount = asset.quantity * asset.price
-			total += amount
-		})
-		
-		// mounts the portfolio JSON
-		json = {total_amount: total}
-		json.assets = assets
-		callback(json)
+
 		/*
 		callback({
 			total_amount:239034.23,
@@ -53,4 +57,11 @@ function getPortfolio(callback) {
 	})
 }
 
+function getPrice(asset, callback) {
+	if (asset.type == 'stock')
+	    assetsModule.getStockPrice(asset.symbol, callback)
+	else
+	    assetsModule.getCryptoPrice(asset.symbol, callback)
+}
+	
 module.exports = { getOrdersForAmount, getPortfolio }
