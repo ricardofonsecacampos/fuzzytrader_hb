0457a5136b0d28ebf16760d8657bd128e7119764
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2020-12-14 08:51:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,38 @@ const fs = require('fs')
 const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
-  console.log(req)
+  console.log(req.url)
   
-  // serve the HTML application page
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('frontend/fuzzytrader-hb.html').pipe(res)
+  let location = 'frontend'
+  let serveFile = true
+  let contentType = 'application/json'
+  
+  switch (req.url)	{
+    case '':
+    case '/':
+      contentType = 'text/html'
+      location += '/fuzzytrader-hb.html'
+      break;
+      
+    case '/styles.css':
+      contentType = 'text/css'
+      location += req.url
+      break;
+      
+    case '/fuzzytrader-hb.js':
+      contentType = 'text/javascript'
+      location += req.url
+      break;
+      
+		case '/get-portfolio':
+      serveFile = false
+      break;
+	}
+
+  // starts writing the response.
+  res.writeHead(200, { 'content-type': contentType })
+  // serve the requested file.
+  if (serveFile) fs.createReadStream(location).pipe(res)
   
 }).listen(PORT)
 
