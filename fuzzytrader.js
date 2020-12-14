c749c3b0dc5f7adaeb365d57bbbde73501109d3f
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2020-12-14 09:23:01 -0300

    Update fuzzytrader.js

--- fuzzytrader.js
+++ fuzzytrader.js
@@ -1,9 +1,5 @@
-//try {
-	const assetsModule = require('./assets')
-	const dbModule = require('./db')
-	const searchAssets = dbModule.searchAssets
-//} catch {
-//}
+const assetsModule = require('./assets')
+const dbModule = require('./db')
 
 // Determines if will be offered lower risk (conservative) or high stakes (agressive) assets to the trader.
 // The rationale is to allow up to 20% of total amount invested in high stakes assets.
@@ -16,7 +12,7 @@ function getOrdersForAmount(portfolioAmount, agressiveAmount, tradeAmount, callb
 	if (((agressiveAmount + tradeAmount) / (portfolioAmount + tradeAmount)) > 0.2)
 		typeSelected = 'conservative'
 
-	searchAssets(typeSelected, callback)
+	dbModule.searchAssets(typeSelected, callback)
 }
 
 // Lists available assets and sets the quantity owned by the trader (in his portfolio).
