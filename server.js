e6123f5f924c13a0022db19ed6b8691aaf6f853d
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2020-12-14 07:55:20 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+  
+// HTTP server to link the application HTML page to services in NodeJS functions.
+
+const http = require('http')
+const fs = require('fs')
+
+// use the port Heroku indicates
+const PORT = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+
+  // serve the HTML application page
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('frontend/fuzzytrader-hb.html').pipe(res)
+  
+}).listen(PORT)
+
+console.log('Node server running on port ' + PORT)
