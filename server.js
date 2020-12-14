f57c7de5947369803814dec2bda783ff2af37286
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2020-12-14 08:10:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const fs = require('fs')
 const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
-
+  console.log(req.path)
+  
   // serve the HTML application page
   res.writeHead(200, { 'content-type': 'text/html' })
   fs.createReadStream('frontend/fuzzytrader-hb.html').pipe(res)
