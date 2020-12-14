f210e59d9977b80012086be26bd41cfad6dd3e67
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2020-12-14 19:44:31 -0300

    Update fuzzytrader-hb.js

--- frontend/fuzzytrader-hb.js
+++ frontend/fuzzytrader-hb.js
@@ -9,10 +9,8 @@ function getOrdersForAmount(amount, callback) {
 			body: {portAmount: 0, portAgresAmount: 0, tradeAmount: amount}
 		})
 		.then(function (response) {
-			console.log(response)
 			response.json()
 			.then(function (assets) {
-				console.log(assets)
 				callback(assets)
 			})
 			.catch(() => {
@@ -22,23 +20,16 @@ function getOrdersForAmount(amount, callback) {
 		.catch(() => {
 			//TODO add error message.
 		})
-	/*
-	callback([
-		{symbol:"AAPL", quantity:(amount/135.66).toFixed(), price:"135.66"},
-		{symbol:"BNB", quantity:(amount/183.05).toFixed(5), price:"183.05"},
-		{symbol:"VALE", quantity:(amount/55.0).toFixed(), price:"55.0"}
-	])
-	*/
 }
 
 
 function getPortfolio(callback) {
 	fetch(baseUrl + 'portfolio/get')
 		.then(function (response) {
-			console.log('from JS: ' + response)
 			response.json()
 			.then(function (portfolio) {
 				callback(portfolio)
+				setPriceAndAmount(portfolio, callback)
 			})
 			.catch(() => {
 				//TODO add error message.
@@ -47,15 +38,9 @@ function getPortfolio(callback) {
 		.catch(() => {
 			//TODO add error message.
 		})
-	/*
-	callback({
-		total_amount:"239,034.23",
-		assets: [
-			{symbol:"KODK", quantity:"9876.66", price:"135.66", amount:"567,855.0"},
-			{symbol:"BTCUSDT", quantity:"7218.05", price:"183.05", amount:"23,423.20"},
-			{symbol:"AAPL", quantity:"45000", price:"183.05", amount:"500,000.05"},
-			{symbol:"XRP", quantity:"0.074657", price:"567855.0", amount:"855.0"}
-		]
+}
+
+function setPriceAndAmount(portfolio, callback) {
+	portfolio.assets.forEach((asset) => {
 	})
-	*/
 }
