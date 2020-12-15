12a97d808dd5b3d1887589c92812b4b846f7ecc7
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2020-12-15 05:45:24 -0300

    Update fuzzytrader-hb.js

--- frontend/fuzzytrader-hb.js
+++ frontend/fuzzytrader-hb.js
@@ -3,11 +3,14 @@ const baseUrl = 'https://fuzzytrader-app.herokuapp.com/'
 function getOrdersForAmount(amount, callback) {
 	if (amount <= 0) return []
 	
-	fetch((baseUrl + 'orders'),
-		{
-			method: "POST",
-			body: {portAmount: 0, portAgresAmount: 0, tradeAmount: amount}
-		})
+	fetch((baseUrl + 'orders'), {
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		method: "POST",
+		body: {portAmount: 0, portAgresAmount: 0, tradeAmount: amount}
+	})
 		.then(function (response) {
 			response.json()
 			.then(function (assets) {
@@ -24,6 +27,22 @@ function getOrdersForAmount(amount, callback) {
 
 function getPortfolio(callback) {
 	fetch(baseUrl + 'portfolio/get')
+		.then(function (response) {
+			response.json()
+			.then(function (portfolio) {
+				callback(portfolio)
+			})
+			.catch(() => {
+				//TODO add error message.
+			})
+		})
+		.catch(() => {
+			//TODO add error message.
+		})
+}
+
+function addToPortfolio(callback) {
+	fetch(baseUrl + 'portfolio/add')
 		.then(function (response) {
 			response.json()
 			.then(function (portfolio) {
